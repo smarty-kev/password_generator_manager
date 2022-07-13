742c6024edb36f475592f0d141e20bda1b2b2188
smarty-kev <86808153+smarty-kev@users.noreply.github.com>
2022-07-13 18:09:19 -0400

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+import random
+
+
+class Requirement:
+    pass
+
+
+class Symbol(Requirement):
+    pass
+
+
+class Letter(Requirement):
+    pass
+
+
+class Number(Requirement):
+    pass
+
+
+class Length(Requirement):
+    pass
+
+
+class CaseSensitivity(Requirement):
+    pass
